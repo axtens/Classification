de442da56feab57221b4a386e694445cf5b13801
Bruce M. Axtens <bugmagnet@outlook.com>
2019-10-11 16:10:58 +0800

    Add project files.

---
+++ Classifier.cs
@@ -0,0 +1,40 @@
+﻿using Google.Cloud.Language.V1;
+
+using Newtonsoft.Json;
+
+using System;
+
+namespace Classification
+{
+    public class Classifier
+    {
+        public static string Classify(string html)
+        {
+            var document = new Document
+            {
+                Type = Document.Types.Type.Html,
+                Content = html
+            };
+            string error;
+            object cargo;
+            try
+            {
+                var client = LanguageServiceClient.Create();
+                ClassifyTextResponse response = client.ClassifyText(document);
+                cargo = response.Categories;
+                error = null;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+                cargo = string.Empty;
+            }
+            return JsonConvert.SerializeObject(new JsonResponse()
+            {
+                Error = error,
+                Cargo = cargo
+            });
+        }
+    }
+}
+
---
+++ JsonResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classification
+{
+    internal class JsonResponse
+    {
+        public string Error { get; set; }
+        public object Cargo { get; set; }
+    }
+}
