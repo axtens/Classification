76eb4212687cf500fdb39e61e3504b50e5765780
Bruce M. Axtens <bugmagnet@outlook.com>
2019-10-15 14:05:45 +0800

    Differentiate between Html and PlainText

--- Classifier.cs
+++ Classifier.cs
@@ -8,6 +8,37 @@ namespace Classification
 {
     public class Classifier
     {
+        public static string ClassifyPlainText(string text)
+        {
+            var document = new Document
+            {
+                Type = Document.Types.Type.PlainText,
+                Content = text
+            };
+            string error;
+            object cargo;
+            try
+            {
+                var client = LanguageServiceClient.Create();
+                ClassifyTextResponse response = client.ClassifyText(document);
+                cargo = response.Categories;
+                error = null;
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+                cargo = string.Empty;
+            }
+            return JsonConvert.SerializeObject(new JsonResponse()
+            {
+                Error = error,
+                Cargo = cargo
+            });
+
+        }
+
+        public static string ClassifyHtml(string html) => Classify(html);
+
         public static string Classify(string html)
         {
             var document = new Document
